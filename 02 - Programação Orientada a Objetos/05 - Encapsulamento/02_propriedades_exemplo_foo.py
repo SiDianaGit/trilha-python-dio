d806605d1559d4e99963f53ea701dea45e2ddd31
SiDianaGit <si_ap@hotmail.com>
2025-08-25 17:28:48 -0300

    Update 02_propriedades_exemplo_foo.py

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
@@ -1,16 +1,19 @@
 class Foo:
     def __init__(self, x=None):
         self._x = x
-
+    
+    # depurador, é uma função que executa antes de tudo
     @property
     def x(self):
-        #convencao de nome de atributo privado. Não deve ser chamado fora do escopo
+        #convencao de nome de atributo privado. Não deve ser chamado fora do escopo da classe
         return self._x or 0
 
+    #modificar o valor de x. Trabalhar como um atributo. Não utilizar o return
     @x.setter
     def x(self, value):
         self._x += value
 
+    #destruir a instancia
     @x.deleter
     def x(self):
         self._x = 0
@@ -18,7 +21,9 @@ class Foo:
 
 foo = Foo(10)
 print(foo.x)
+
 del foo.x
 print(foo.x)
-foo.x = 10
+
+foo.x = 100
 print(foo.x)
