b13626acb52b9c7ba152821002035fb6957d7934
SiDianaGit <si_ap@hotmail.com>
2025-08-25 18:05:25 -0300

    Update 01_metodos_classe_estatico.py

--- 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
+++ 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
@@ -3,12 +3,13 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
 
-    #depurador que transforma a instancia para metodo de classe. Onde cls é a referencia para a classe
+    #depurador que transforma a instancia para metodo de classe. Onde cls é a referencia para a classe. Precisa do contexto da classe
     @classmethod
     def criar_de_data_nascimento(cls, ano, mes, dia, nome):
         idade = 2022 - ano
         return cls(nome, idade)
 
+    #metodo estatico, não precisa do contexto da classe
     @staticmethod
     def e_maior_idade(idade):
         return idade >= 18
