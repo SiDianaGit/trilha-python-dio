cb2ac0c854d833edbcc18d91a29b7ca552cfeb02
SiDianaGit <si_ap@hotmail.com>
2025-08-25 16:09:33 -0300

    Update 01_heranca_simples.py

--- 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
@@ -7,20 +7,22 @@ class Veiculo:
     def ligar_motor(self):
         print("Ligando o motor")
 
+    #exibir a instancia/objeto
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
-
+#classe Motocicleta é filha da clase Veiculo
 class Motocicleta(Veiculo):
     pass
 
-
+#classe Carro é filha da clase Veiculo
 class Carro(Veiculo):
     pass
 
-
+#classe Caminhão é filha da clase Veiculo
 class Caminhao(Veiculo):
     def __init__(self, cor, placa, numero_rodas, carregado):
+        #o metodo super() traz as caractisticas do objeto pai, pois ao explicitar os atributos os mesmos foram sobrepostos
         super().__init__(cor, placa, numero_rodas)
         self.carregado = carregado
 
