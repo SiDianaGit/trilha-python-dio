ec45cfd9ef8aa10ba9fa61ed51081de7a516c663
SiDianaGit <si_ap@hotmail.com>
2025-08-25 15:56:30 -0300

    Update 01_construtores_destrutores.py

--- 02 - Programação Orientada a Objetos/03 - Construtores e destrutores/01_construtores_destrutores.py
+++ 02 - Programação Orientada a Objetos/03 - Construtores e destrutores/01_construtores_destrutores.py
@@ -1,10 +1,13 @@
 class Cachorro:
+    # método construtor/inicializador. Sempre é executado no inicio para instanciar
     def __init__(self, nome, cor, acordado=True):
         print("Inicializando a classe...")
+        #atributos
         self.nome = nome
         self.cor = cor
         self.acordado = acordado
 
+    #método destrutorv"del". Destroi a instancia. Sempre é executado no final da execução. É menos usado pois o python tem a função garbage 
     def __del__(self):
         print("Removendo a instância da classe.")
 
@@ -22,6 +25,7 @@ c.falar()
 
 print("Ola mundo")
 
+# forçar a destruição da instancia de um objeto
 del c
 
 print("Ola mundo")
